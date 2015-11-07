4cd802fe1d2ed90f78ff4d890417f64e5f36ac54
thegeekyone <x3m34life@gmail.com>
2015-11-07 18:49:12 -0500

    adding some layout

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,3 +1,15 @@
 module LayoutHelper
+  BASE_TITLE = "#{I18n.t('app_title')} - #{I18n.t('app_tagline')}"
+
+  def full_title(page_title=nil)
+    title = ''
+    title += "#{page_title} :: " if page_title.present?
+    title += BASE_TITLE
+    title
+  end
+
+  def title(title_content=nil)
+    content_for :page_title, title_content
+  end
 
 end
