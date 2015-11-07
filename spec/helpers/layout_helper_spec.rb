47956e38a5927f5b0843a9cfd5c7687eca4d09b5
thegeekyone <x3m34life@gmail.com>
2015-11-07 16:50:57 -0500

    testing some layout on spec

---
+++ spec/helpers/layout_helper_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe LayoutHelper do
+  describe '#full_title' do
+    it 'displays only the app title and tagline when no page title is given' do
+      expect(helper.full_title).
+          to eq("#{I18n.t('app_title')} - #{I18n.t('app_tagline')}")
+    end
+
+    it 'includes the page title when one is passed in' do
+      expect(helper.full_title('Blah')).to include('Blah')
+    end
+
+    it 'includes a double colon if a page title is passed in' do
+      expect(helper.full_title('Blah lol wtf')).to include('::')
+    end
+  end
+
+  describe '#title' do
+    it 'creates a content_for title with the content passed in' do
+      helper.title('Blah blah lol man')
+      expect(helper.content_for(:page_title)).to eq('Blah lol wtf')
+    end
+  end
+end
